8bccefdf8a96669f5b7fc060b77b3db091ac945f
DESKTOP-BTNQ693\PC07 <foxworker86@daerisoft.com>
2022-09-16 14:03:42 +0900

    유니티 싱글톤

---
+++ Script/Singleton/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singleton<T> where T : new() {
+
+	private static T _instance = default(T);
+
+	public static T instance 
+	{
+		get 
+		{
+			if (_instance == null)
+			{
+				_instance = new T();
+			}
+
+			return _instance;
+		}
+	}
+}
